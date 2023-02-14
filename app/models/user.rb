3e0aa725b703a3f61597583a6ced15d4bcac9627
Alexander Veselov <collabsis.g@gmail.com>
2023-02-14 14:50:17 +0400

    Apply rubocop rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
-  
-  @email_regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  
-  validates :first_name, :last_name, presence:true
+
+  @email_regex = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
+
+  validates :first_name, :last_name, presence: true
   validates :tabel_id, presence: true, uniqueness: true
-  validates :password, presence:true, length: {minimum:8}
-  validates :email, 
-    presence: true,
-    uniqueness: true,
-    format: { with: @email_regex }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: @email_regex }
 end
--- test/models/engineer_test.rb
+++ test/models/engineer_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class EngineerTest < ActiveSupport::TestCase
   # test "the truth" do
