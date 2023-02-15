be0bbba2aac72a46728604d230dab75eb2698fa2
Alexander Veselov <collabsis.g@gmail.com>
2023-02-15 11:56:48 +0400

    Added device rg model and test

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,8 +1,6 @@
 require 'resolv'
 
 class Server < ApplicationRecord
-  @ip_regex = /^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/
-
   belongs_to :service
   belongs_to :room
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_070609) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_075110) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_070609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["organization_id"], name: "index_buildings_on_organization_id"
+  end
+
+  create_table "device_reg_groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "divisions", force: :cascade do |t|
