45eeaca73043005c641d1563033e4049211b893e
Alexander Veselov <collabsis.g@gmail.com>
2023-02-22 11:53:38 +0400

    Rebuilded project, added bootstrap and esbuild

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def show; end
+
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'home#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :home
+  resources :armstrong, only: [:index, :show]
+  resources :about, only: [:index]
+  resources :device, only: [:index, :show]
 end
