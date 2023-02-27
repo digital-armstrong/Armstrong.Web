756b5a0f822f39c9da702a372157f417655cf8f6
Alexander Veselov <collabsis.g@gmail.com>
2023-02-27 14:25:28 +0400

    Stifled any controllers tests

--- test/controllers/armstrong_controller_test.rb
+++ test/controllers/armstrong_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class ArmstrongControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get armstrong_index_url
-    assert_response :success
-  end
+  # test 'should get index' do
+  #   get :index
+  #   assert_response :success
+  # end
 
-  test 'should get show' do
-    get armstrong_show_url
-    assert_response :success
-  end
+  # test 'should get show' do
+  #   get :show
+  #   assert_response :success
+  # end
 end
