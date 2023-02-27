146bfce8f3ff1efe8f2de6edd7ed8c050873bf5f
Alexander Veselov <collabsis.g@gmail.com>
2023-02-27 20:37:41 +0400

    Added new and edit atcions for device

--- app/controllers/device_controller.rb
+++ app/controllers/device_controller.rb
@@ -8,4 +8,12 @@ class DeviceController < ApplicationController
       page(params[:page]).
       per(params[:per_page])
   end
+
+  def edit
+    @device = Device.find(params[:id])
+  end
+
+  def new
+    @device = Device.new
+  end
 end
