827c1901abe8b3597655f4b52132891f5e7a160a
ilya <seaatsky.g@gmail.com>
2023-03-06 01:03:34 +0400

    fix device years validation

--- app/models/device.rb
+++ app/models/device.rb
@@ -9,6 +9,9 @@ class Device < ApplicationRecord
 
   validates :inventory_id, numericality: { less_than_or_equal_to: 2147483647 }, presence: true, uniqueness: true
   validates :serial_id, :tabel_id, presence: true, uniqueness: true
+  validates :year_of_commissioning, :year_of_production,
+            numericality: { in: 1900..3000, message: 'должен быть больше или равен 1900 и меньше 3001' }, allow_nil: true
+  validates :year_of_production, presence: true
 
   def self.ransackable_attributes(_auth_object = nil)
     ['created_at', 'device_model_id', 'device_reg_group_id', 'id', 'inventory_id', 'serial_id', 'tabel_id', 'updated_at',
