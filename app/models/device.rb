6edde35505353a9e191a43cefd6d91670f2749b4
Alexander Veselov <collabsis.g@gmail.com>
2023-03-06 08:40:32 +0400

    Added year validation

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,10 @@
+require 'date'
+
 class Device < ApplicationRecord
   include ExportPdf
+
+  current_year = Date.today.year
+  year_error_msg = "должен быть больше 1900 и меньше или равен #{current_year}"
 
   belongs_to :device_model
   belongs_to :device_reg_group
@@ -9,6 +14,8 @@ class Device < ApplicationRecord
 
   validates :inventory_id, numericality: { less_than_or_equal_to: 2147483647 }, presence: true, uniqueness: true
   validates :serial_id, :tabel_id, presence: true, uniqueness: true
+  validates :year_of_commissioning, :year_of_production, numericality: { in: 1900..current_year, message: year_error_msg }, allow_nil: true
+  validates :year_of_production, presence: true
 
   def self.ransackable_attributes(_auth_object = nil)
     ['created_at', 'device_model_id', 'device_reg_group_id', 'id', 'inventory_id', 'serial_id', 'tabel_id', 'updated_at',
