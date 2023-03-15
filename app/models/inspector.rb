7fbcbe16fc31ea4e673f609dde0fa104c95a8905
Alexander Veselov <collabsis.g@gmail.com>
2023-03-15 13:54:24 +0400

    BREAKING CHANGE: Deleted user STI models

--- app/models/inspector.rb
+++
@@ -1,3 +0,0 @@
-class Inspector < User
-  has_many :inspections
-end
--- test/models/engineer_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class EngineerTest < ActiveSupport::TestCase
-  test 'create_user_model_engineer' do
-    engineer = create(:engineer)
-    assert engineer.persisted?
-  end
-end
