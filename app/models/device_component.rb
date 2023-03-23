0cc4cc58b7d60af9f548b0020148726c7bd64dbc
Alexander Veselov <collabsis.g@gmail.com>
2023-03-23 11:27:52 +0400

    Added device componet model

--- app/models/device_component.rb
+++ app/models/device_component.rb
@@ -1,3 +1,11 @@
 class DeviceComponent < ApplicationRecord
   belongs_to :supplementary_kit
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "id", "measurement_max", "measurement_min", "measuring_unit", "name", "serial_id", "supplementary_kit_id", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["supplementary_kit"]
+  end
 end
