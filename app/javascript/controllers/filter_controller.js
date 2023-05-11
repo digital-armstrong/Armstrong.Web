c6b6a6ebfc71e6f86ca95b8b23a349afc31cb0b1
ilya <seaatsky.g@gmail.com>
2023-05-11 06:23:12 +0400

    фикс фильтра для моделей

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -4,42 +4,47 @@ import ky from "ky";
 
 export default class extends Controller {
   connect() {
-    let $filtrator = $(this.element).attr('data-filtrator-class')
-    this.doFilter(`#${$filtrator}`);
+    let $filtrator = $(this.element).attr("data-filtrator-class");
+    this.doFilter(`#${$filtrator}`, $(this.element));
   }
 
-  disconnect() {
-  }
+  disconnect() {}
 
   filter(obj) {
-    this.doFilter($(obj.target));
+    this.doFilter($(obj.target), obj);
   }
 
-  async doFilter(obj) {
-    console.log(obj)
-      let $obj = $(obj);
-      let id = $($obj).val();
-      let to_filter_id = $obj.attr("data-to-filter-class");
-      $.ajax({
-        type: "GET",
-        url: `/api/v1/filters?id=${id}`,
-        success: function (data) {
-          var $to_filter = $(`#${to_filter_id}`);
-          $to_filter.prop("disabled", true);
-          $to_filter.empty();
-          if ($obj.attr("data-filtrator-class") == 'filtrator-class-index') {
-            $to_filter.append($("<option/>", { value: "" }).text("Не учитывать"));
-          }
-          for (var i = 0; i < data.length; i++) {
-            $to_filter.append(
-              $("<option/>", { value: data[i].id }).text(data[i].name)
-            );
-          }
-          $to_filter.prop("disabled", false);
-        },
-        failure: function (data) {
-          console.log(data);
-        },
-      });
+  async doFilter(obj, fullObj) {
+    let $obj = $(obj);
+    let id = $($obj).val();
+    let to_filter_id = $obj.attr("data-to-filter-class");
+    $.ajax({
+      type: "GET",
+      url: `/api/v1/filters?id=${id}`,
+      success: function (data) {
+        var $to_filter = $(`#${to_filter_id}`);
+        $to_filter.prop("disabled", true);
+        $to_filter.empty();
+        
+        if ($obj.attr("data-filtrator-class") == "filtrator-class-index") {
+          $to_filter.append($("<option/>", { value: "" }).text("Не учитывать"));
+        }
+        for (var i = 0; i < data.length; i++) {
+          $to_filter.append(
+            $("<option/>", { value: data[i].id }).text(data[i].name)
+          );
+        }
+
+        let prevMeasurementClassVal = $(fullObj).attr("data-prev-mc-val");
+        if (prevMeasurementClassVal != null) {
+          $to_filter.val(prevMeasurementClassVal);
+        }
+
+        $to_filter.prop("disabled", false);
+      },
+      failure: function (data) {
+        console.log(data);
+      },
+    });
   }
 }
