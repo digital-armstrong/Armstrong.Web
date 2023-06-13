2d6cdfed1cfb2af99cbbbebeee0fcc15372e58e6
Alexander Veselov <collabsis.g@gmail.com>
2023-06-13 19:51:15 +0400

    Fixed wrong word

--- app/jobs/check_device_status_job.rb
+++ app/jobs/check_device_status_job.rb
@@ -3,14 +3,14 @@ class CheckDeviceStatusJob
 
   def perform
     Device.find_each do |device|
-      seporator = Date.today + 1.month
+      separator = Date.today + 1.month
       current_date = Date.today
 
       unless device.last_successful_inspection_raw.nil?
         next_inspection_date = device.last_successful_inspection_raw + 1.year
-        status = if next_inspection_date > seporator
+        status = if next_inspection_date > separator
                    Device::INSPECTION_EXPIRATION_STATUS[:verified]
-                 elsif next_inspection_date <= seporator && next_inspection_date > current_date
+                 elsif next_inspection_date <= separator && next_inspection_date > current_date
                    Device::INSPECTION_EXPIRATION_STATUS[:prepare_to_inspection]
                  else
                    Device::INSPECTION_EXPIRATION_STATUS[:expired]
