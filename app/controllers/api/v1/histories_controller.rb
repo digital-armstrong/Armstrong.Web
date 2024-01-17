36511b36434b021239b50582c8b11a8fdd7d950b
Alexander Veselov <collabsis.g@gmail.com>
2024-01-17 14:04:12 +0400

    UPD: возвращает последние 100 записей в истории для {id} канала

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -3,7 +3,7 @@ module Api
     before_action :set_channel, only: [:show]
 
     def show
-      histories = History.select { |hs| hs.channel_id == @channel_id }
+      histories = History.select { |hs| hs.channel_id == @channel_id }.last(100)
 
       result = histories.sort { |a, b| a[:event_datetime] <=> b[:event_datetime] }
 
