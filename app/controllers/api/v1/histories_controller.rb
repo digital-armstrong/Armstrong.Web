3104835afbda7c197c0f0b6d7202a55780c180f7
Alexander Veselov <collabsis.g@gmail.com>
2024-03-04 12:13:25 +0400

    UPD: добавляет параметризированные запросы на API (start_datetime end_datetime)

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -3,18 +3,23 @@ module Api
     before_action :set_channel, only: [:show]
 
     def show
-      histories = History.select { |hs| hs.channel_id == @channel_id }.last(100)
+      histories = if params[:start_datetime].present? && params[:end_datetime].present?
+                    History.where(event_datetime: params[:start_datetime]..params[:end_datetime],
+                                  channel_id: @channel.id)
+                  else
+                    History.select { |hs| hs.channel_id == @channel.id }.last(100)
+                  end
 
       result = histories.sort { |a, b| a[:event_datetime] <=> b[:event_datetime] }
-
-      render(json: result)
+      render(json: result, except: [:created_at, :updated_at])
     end
 
     private
 
     def set_channel
       @channel = Channel.find(params[:id])
-      @channel_id = @channel.id
+    rescue ActiveRecord::RecordNotFound => e
+      render(json: { error: e }, status: :not_found)
     end
   end
 end
