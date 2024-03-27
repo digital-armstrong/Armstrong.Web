44e9c305f96f580f2684f735d9039c985d373000
Alexander Veselov <collabsis.g@gmail.com>
2024-03-27 22:55:25 +0400

    UPD: обновляет вызов API на index роут с параметрами

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -2,6 +2,16 @@ module Api
   class V1::HistoriesController < ApplicationController
     before_action :set_channel, only: [:show]
 
+    def index
+      histories = if params[:start_datetime].present? && params[:end_datetime].present?
+                    History.where(event_datetime: params[:start_datetime]..params[:end_datetime])
+                  else
+                    []
+                  end
+
+      render(json: histories, except: [:id, :event_impulse_value, :event_not_system_value, :created_at, :updated_at])
+    end
+
     def show
       histories = if params[:start_datetime].present? && params[:end_datetime].present?
                     History.where(event_datetime: params[:start_datetime]..params[:end_datetime],
