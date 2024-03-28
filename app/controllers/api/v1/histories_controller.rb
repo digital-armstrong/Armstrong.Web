b6e66c8bc38777b8cf0c017356682eb2b1a985d0
Alexander Veselov <collabsis.g@gmail.com>
2024-03-28 11:47:03 +0400

    ADD: добавляет лимит на размер временного окна выборки по всей таблице History

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -3,10 +3,15 @@ module Api
     before_action :set_channel, only: [:show]
 
     def index
-      histories = if params[:start_datetime].present? && params[:end_datetime].present?
+      datetime_interval = diff_hours(params[:start_datetime], params[:end_datetime])
+      histories = if params[:start_datetime].present? && params[:end_datetime].present? && datetime_interval < 2
                     History.where(event_datetime: params[:start_datetime]..params[:end_datetime])
                   else
-                    []
+                    {
+                      code: 429,
+                      message: 'Parameters is not a valid or range of the datetime is too long.',
+                      hint: 'The interval between start_datetime and end_datetime should be no more than two hours.',
+                    }.to_json
                   end
 
       render(json: histories, except: [:id, :event_impulse_value, :event_not_system_value, :created_at, :updated_at])
@@ -30,6 +35,13 @@ module Api
       @channel = Channel.find(params[:id])
     rescue ActiveRecord::RecordNotFound => e
       render(json: { error: e }, status: :not_found)
+    end
+
+    # Returns the integer value of the difference
+    # between the start and end datetime values
+    def diff_hours(start_date, end_date)
+      diff_in_seconds = end_date.to_time - start_date.to_time
+      (diff_in_seconds / (60**2)).to_i
     end
   end
 end
